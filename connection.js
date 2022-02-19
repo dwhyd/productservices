8841359b933fbe79336bc28da9f5801229ed7869
Diki Wahyudi <68983872+dwhyd@users.noreply.github.com>
2022-02-19 13:25:43 +0700

    Update connection.js

--- connection.js
+++ connection.js
@@ -4,7 +4,7 @@ const url = "mongodb://diki:1234@localhost:27017/latihan?authSource=admin";
 mongoose.connect(url, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
+//   useCreateIndex: true,
 });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
